01b8e671e2fbda29d31816bc24029d2772031a38
Admin <Admin>
2017-05-23 10:04:11 +0530

    chahana initial commit

---
+++ application/controllers/test.php
@@ -0,0 +1,16 @@
+<?php 
+   class Test extends CI_Controller {
+  
+    //   public function index() { 
+    //      echo "This is default function."; 
+    //   } 
+    // $this->load->view('directory-name/name');
+    //   public function hello() { 
+    //      echo "This is hello function."; 
+    //   } 
+
+     public function index() { 
+         $this->load->view('test'); 
+      } 
+   } 
+?>
